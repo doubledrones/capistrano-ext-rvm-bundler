9b902b4ea3c0c805193d4c95dcb5184cbf089897
Marcin Nowicki <pr0d1r2@gmail.com>
2011-11-14 18:59:38 +0100

    Run tasks only on application servers.

--- lib/capistrano/ext/rvm-bundler.rb
+++ lib/capistrano/ext/rvm-bundler.rb
@@ -4,26 +4,26 @@ Capistrano::Configuration.instance.load do
 
   namespace :rubygems do
     desc "update rubygems in RVM environment"
-    task :update do
+    task :update, :roles => :app, :except => { :no_release => true }  do
       run "rvm '#{rvm_ruby_string}' && gem update --system"
     end
   end
 
   namespace :bundler do
     desc "install bundler gem in RVM environment"
-    task :install do
+    task :install, :roles => :app, :except => { :no_release => true }  do
       run "rvm '#{rvm_ruby_string}' && gem install bundler"
     end
   end
 
   namespace :bundle do
     desc "install bundle in RVM environment"
-    task :install do
+    task :install, :roles => :app, :except => { :no_release => true }  do
       run "cd #{latest_release} && rvm '#{rvm_ruby_string}' && bundle install --gemfile #{latest_release}/Gemfile --quiet --without development test"
     end
 
     desc "reset bundle in RVM environment"
-    task :reset do
+    task :reset, :roles => :app, :except => { :no_release => true }  do
       run "cd #{current_path} && rvm '#{rvm_ruby_string}' && rm -f Gemfile.lock && rm -rf `rvm gemdir` && gem install bundler && bundle install --gemfile #{current_path}/Gemfile --without development test"
     end
   end
